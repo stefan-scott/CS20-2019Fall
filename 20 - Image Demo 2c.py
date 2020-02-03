de90adc0d6451b2c01e59d6bf75a4bc45489b4ad
stefan-scott <scottst@spsd.sk.ca>
2020-02-03 16:17:05 -0600

    image demos part 2

--- 20 - Image Demo 2c.py
+++ 20 - Image Demo 2c.py
@@ -1,12 +1,12 @@
 import image
 
-source_image = image.Image("rooster.jpg") #open an image
-add_image = image.Image("smile.png")
+source_image = image.Image("moon.jpg") #open an image
+THRESHOLD = 50
 
 width = source_image.get_width()   #find the width and
 height = source_image.get_height()  #height of the image
-add_width = add_image.get_width()   #width of emoji
-add_height = add_image.get_height() #height of the emoji
+
+shifted = image.EmptyImage(width,height)
 
 window = image.ImageWin(width, height)  #create a window
 
@@ -14,30 +14,30 @@ source_image.draw(window)
 
 #Set up Loops to Visit every pixel and do some work
 
-for x in range(add_width):
-    for y in range(add_height):
+for x in range(1,width):
+    for y in range(1,height):
         #retrieve the current pixel
-        current_pixel = add_image.get_pixel(x,y)
-        dest_pixel = source_image.get_pixel(x+155,y+100)
+        current_pixel = source_image.get_pixel(x,y)
+        shifted.set_pixel(x-2,y-2,current_pixel)
         
-        #access the R,G,B component values
-        r = current_pixel.get_red()
-        g = current_pixel.get_green()
-        b = current_pixel.get_blue()
-        
-        r2 = dest_pixel.get_red()
-        g2 = dest_pixel.get_green()
-        b2 = dest_pixel.get_blue()
-        
-        red = r * 0.5 + r2 * 0.5
-        green = g * 0.5 + g2 * 0.5
-        blue = b * 0.5 + b2 * 0.5
-        
-        new_pixel = image.Pixel(red, green, blue)
-        
-        if r<250 and g<250 and b<250:
-            source_image.set_pixel(x+155,y+100,new_pixel) #create a new pixel
     
+#     if x % 3 == 0: #to speed up the animation
+#         source_image.draw(window)  #animate the manipulation
+shifted.draw(window)            
+
+
+for x in range(width):
+    for y in range(height):
+        pixel_one = source_image.get_pixel(x,y)
+        pixel_two = shifted.get_pixel(x,y)
+        
+        avg_one = (pixel_one.get_red() + pixel_one.get_green() + pixel_one.get_blue()) / 3
+        avg_two = (pixel_two.get_red() + pixel_two.get_green() + pixel_two.get_blue()) / 3
+        
+        if abs(avg_one - avg_two) > THRESHOLD:
+            source_image.set_pixel(x,y,image.Pixel(255,0,0))
+        #else:
+            #source_image.set_pixel(x,y,image.Pixel(255,255,255))
     if x % 3 == 0: #to speed up the animation
-        source_image.draw(window)  #animate the manipulation
-source_image.draw(window)            
+        source_image.draw(window)
+source_image.draw(window)
